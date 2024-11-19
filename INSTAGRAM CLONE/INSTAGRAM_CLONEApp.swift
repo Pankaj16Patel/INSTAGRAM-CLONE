7bd35200d7d7ed34865f217d5e648da4bb4d8f50
Pankaj <pankajpatel162000@gmail.com>
2024-11-19 12:36:07 +0530

    Initial Commit

---
+++ INSTAGRAM CLONE/INSTAGRAM_CLONEApp.swift
@@ -0,0 +1,17 @@
+//
+//  INSTAGRAM_CLONEApp.swift
+//  INSTAGRAM CLONE
+//
+//  Created by amarjeet patel on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct INSTAGRAM_CLONEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
